dd2a2b93868679ed3d452713690a65926c75ebc8
Naile Hamidova <nailehamidova@Ravis-MacBook-Air.local>
2025-04-22 04:44:38 +0400

    İlkin commit

---
+++ Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Threading.Tasks;
+
+public class Program{
+
+    public static async Task Main(string[] args)
+    {
+        //Task 1 !!!
+    // int a=int.Parse(Console.ReadLine());
+    // int b=int.Parse(Console.ReadLine());
+    // int c=int.Parse(Console.ReadLine());
+    // if(a<1000 && b<1000 && c<1000){
+    //     if(a>b && a<c || a<b && a>c){
+    //         Console.WriteLine(a);
+    //     }
+    //     else if(b>a && b<c || b<a && b>c){
+    //         Console.WriteLine(b);
+    //     }
+    //     else{
+    //         Console.WriteLine(c);
+    //     }
+    // }
+    // else{
+    //     Console.WriteLine("Duzgun daxil et! ");
+    // }
+
+
+    //Task 2 !!!!
+    // int a=int.Parse(Console.ReadLine());
+    // int b=int.Parse(Console.ReadLine());
+    // int c=int.Parse(Console.ReadLine()); 
+    //  if(a<100 && b<100 && c<100){
+    // if(a != b && a != c && b != c){
+    //    Console.WriteLine("3");
+    // }
+    // else if(a == b && b == c){
+    //     Console.WriteLine("1");
+    // }
+    // else{
+    //     Console.WriteLine("2");
+    // }}
+    // else{
+    //     Console.WriteLine("Duzgun daxil et! ");
+    // }
+
+    // Task 3!!!
+    // int a=int.Parse(Console.ReadLine());
+    // int b=int.Parse(Console.ReadLine());
+    // int c=int.Parse(Console.ReadLine()); 
+    //  if(a<1000 && b<1000 && c<1000){
+    // if(a > b && a > c && a == Math.Sqrt(Math.Pow(b,2)+Math.Pow(c,2))){
+
+    //    Console.WriteLine("YES");
+    // }
+    // else if(b>a && b>c && b == Math.Sqrt(Math.Pow(a,2)+Math.Pow(c,2))){
+    //    Console.WriteLine("YES");
+    // }
+    // else if(c>a && c> b && c == Math.Sqrt(Math.Pow(b,2)+Math.Pow(a,2))){
+    //    Console.WriteLine("YES");
+    // } 
+    // else{
+    //     Console.WriteLine("NO");
+    // }
+    // }
+
+    // else{
+    //     Console.WriteLine("Duzgun daxil et! ");
+    // }
+
+
+// Task 4 !!!!
+// int a=int.Parse(Console.ReadLine());
+// if(a>2 && a<6){
+//     Console.WriteLine("Spring");
+// }
+// else if(a>5 && a<9){
+//     Console.WriteLine("Summer");
+// }
+// else if(a>8 && a<12){
+//     Console.WriteLine("Autumn");
+// } 
+// else{
+//     Console.WriteLine("Winter");
+// }
+
+
+
+//Task 5 !!!
+
+    int a=int.Parse(Console.ReadLine());
+    int b=int.Parse(Console.ReadLine());
+    int c=int.Parse(Console.ReadLine()); 
+     if(a<10000 && b<10000 && c<10000){
+    if(a % 2 != 0 || b % 2 != 0 || c % 2 != 0  &&  a % 2 == 0 || b % 2 == 0 || c % 2 == 0){
+       Console.WriteLine("YES");
+    }
+
+    else{
+        Console.WriteLine("NO");
+    }}
+    else{
+        Console.WriteLine("Duzgun daxil et! ");
+    }  
+    
+    }}
